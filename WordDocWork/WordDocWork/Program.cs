b14d75137deaa71551595c2d5813ab10ed3074f5
beobload <pheelen@yandex.ru>
2021-01-26 19:19:19 +0700

    Объявление полей и локальных переменных (2)
    Объявление полей и локальных переменных (2)

--- WordDocWork/WordDocWork/Program.cs
+++ WordDocWork/WordDocWork/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Office.Interop.Word;
 
 namespace WordDocWork
 {
     class Program
     {
+
+        /// <summary>
+        /// номера раздела, ==0 - нет разделов
+        /// </summary>
+        static uint _sectionNumber = 0;
+        /// <summary>
+        /// номера рисунков, ==0 - нет картинок
+        /// </summary>
+        static uint _pictureNumber = 0;
+        /// <summary>
+        /// номера таблиц, ==0 - нет таблиц
+        /// </summary>
+        static uint _tableNumber = 0;
+
         static void Main(string[] args)
         {
+
+            string sourcePath = @"f:\stud_work\шаблон.rtf";//путь до исходного шаблона
+            string distPath = @"f:\stud_work\result.rtf";//путь до выходного файла
+            string csvPath = @"f:\stud_work\data.csv";//путь до csv файла для создания таблицы
+
+            //список закладок
+            string[] templateStringList =
+                {
+                "[*имя раздела*]",///0
+                "[*имя рисунка*]",///1
+                "[*ссылка на следующий рисунок*]",///2
+                "[*ссылка на предыдущий рисунок*]",///3
+                "[*ссылка на таблицу*]",///4
+                "[*таблица первая*]"///5
+                };
+
         }
     }
 }
