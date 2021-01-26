55cea330b89b465fa6f91bb78ac072b9a9499cf4
beobload <pheelen@yandex.ru>
2021-01-26 20:40:23 +0700

    Форматирование (4)
    Форматирование (4)

--- WordDocWork/WordDocWork/Program.cs
+++ WordDocWork/WordDocWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Office.Interop.Word;
+using System;
 
 namespace WordDocWork
 {
@@ -35,13 +36,75 @@ namespace WordDocWork
                 "[*ссылка на таблицу*]",///4
                 "[*таблица первая*]"///5
                 };
-
             var application = new Application();
             application.Visible = true;
 
             var document = application.Documents.Open(sourcePath);
 
+            Paragraph prevParagraph = null;
+            foreach (Paragraph paragraph in document.Paragraphs)
+            {
+                for (int i = 0; i < templateStringList.Length; i++)
+                {
+                    if (paragraph.Range.Text.Contains(templateStringList[i]))
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                {
+                                    paragraph.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                                    paragraph.Range.Font.Name = "Times New Roman";
+                                    paragraph.Range.Font.Size = 15;
+                                    paragraph.Format.SpaceAfter = 12;
+                                    paragraph.Range.Font.Bold = 1;
+                                    paragraph.Range.HighlightColorIndex = 0;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    paragraph.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                                    paragraph.Range.Font.Name = "Times New Roman";
+                                    paragraph.Range.Font.Size = 12;
+                                    paragraph.Format.SpaceAfter = 12;
+                                    paragraph.Range.HighlightColorIndex = 0;
+
+                                    if (prevParagraph != null)
+                                    {
+                                        prevParagraph.Format.SpaceBefore = 12;
+                                        prevParagraph.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+
+                                }
+                                break;
+                            case 3:
+                                {
+
+                                }
+                                break;
+                            case 4:
+                                {
+
+                                }
+                                break;
+                            case 5:
+                                {
+
+                                }
+                                break;
+
+                        }
+                    }
+                }
+                prevParagraph = paragraph;
+            }
+
             document.SaveAs2(distPath);
+            System.Console.In.Read();
+            // application.Quit();
         }
     }
 }
